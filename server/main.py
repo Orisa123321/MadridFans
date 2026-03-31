def785c4ff2eb22c5bf76b572b2b608fc60a0891
Orisa123321 <136339149+Orisa123321@users.noreply.github.com>
2026-03-31 12:11:31 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- server/main.py
+++ server/main.py
@@ -31,8 +31,7 @@ API_CACHE = {}
 def fetch_from_api(endpoint):
     # שים פה את ה-URL והטוקן/API KEY האמיתיים שלך
     url = f"http://api.football-data.org/{endpoint}" 
-    headers = {"X-Auth-Token": "96dd89fde61c7291a454ac842a3967a4
-"} 
+    headers = {"X-Auth-Token": "96dd89fde61c7291a454ac842a3967a4"} 
     
     try:
         # הקסם פה: timeout=5. אם אין תשובה תוך 5 שניות, מנתקים!
