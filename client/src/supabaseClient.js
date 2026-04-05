814bf9d798ad5d8d2cbbe9dab90326e7cdcd95e9
Orisa123321 <136339149+Orisa123321@users.noreply.github.com>
2026-04-06 00:30:02 +0300

    Update Supabase anon key in supabaseClient.js

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://yjdkyjppeiimxxrlwdly.supabase.co';
-const supabaseAnonKey = 'sb_secret_d17qyLhtHWKKKKDHIFtAug_FhBLi77p';
+const supabaseAnonKey = 'sb_publishable_UAmYKRGvuWHL5bk54ttD4g_TRTKPzfo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
